e074f6489299d8dcdf2c63e95d0842d458e0d3b5
kyungmin kim <ee5201@naver.com>
2023-04-20 21:02:32 +0900

    updateStudy

---
+++ 08-01-docker-compose-with-mongo/backend/congig.js
@@ -0,0 +1,11 @@
+// export const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title:'나만의 미리 프로젝트 API명세서',
+//       verstion :'1.0.0',
+//     },
+//   },
+//   apis:['./swagger/*swagger.js'],
+
+// }
