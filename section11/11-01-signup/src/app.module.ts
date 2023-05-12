b0b9e2fc54203f7db059f6e35d48bbce5214642b
kyungmin kim <ee5201@naver.com>
2023-05-12 17:43:01 +0900

    updatestudy

---
+++ section11/11-01-signup/src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { BoardsMoudle } from './apis/boards/boards.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './apis/boards/entities/board.entity';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './apis/products/products.module';
+import { ProductsCategoriesModule } from './apis/productsCategories/productsCategories.module';
+import { UsersMoudle } from './apis/users/user.module';
+
+@Module({
+  imports: [
+    BoardsMoudle,
+    ProductsModule,
+    ProductsCategoriesModule,
+    UsersMoudle,
+    ConfigModule.forRoot(), // TypeormModule위에 무조건 있어야 한다.
+    // 그 이유는 읽지 못하기 때문이다. configModule은 env를 읽기 위해서 nest에서 사용하는거 원래는 Dotenv라이브러리 다운 받아서 사용한다.
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql', //mysql 이 맞아 너가 몰라서 그래 사용할 때 as mysql
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'], //__dirname:현위치  **모든 폴더안에 *.entity들어가있는 파일들만 모두 등록
+      synchronize: true, // Board를 불러오기위해 동기화 하기위해 설정하는것
+      logging: true, // debug역활
+    }),
+  ],
+})
+export class AppModule {}
