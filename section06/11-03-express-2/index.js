ac40473c95f9c1b8ecc8f3c9aabff83bb0eaeef6
kyungmin kim <ee5201@naver.com>
2023-06-10 14:48:32 +0900

    updateStudy

---
+++ section06/11-03-express-2/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.post("/products/buy", (req, res) => {
+  res.send;
+});
+
+app.post("/prodcuts/refund", (req, res) => {
+  res.send;
+});
+
+app.listen(3000);
