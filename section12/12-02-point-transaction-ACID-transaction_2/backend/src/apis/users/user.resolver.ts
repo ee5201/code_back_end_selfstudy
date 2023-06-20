232b6b5371e9046b31dbd7de1b5bb8948f9e7025
kyungmin kim <ee5201@naver.com>
2023-06-20 22:52:54 +0900

    updateStudy

---
+++ section12/12-02-point-transaction-ACID-transaction_2/backend/src/apis/users/user.resolver.ts
@@ -0,0 +1,36 @@
+import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UsersService } from './user.service';
+import { User } from './entities/user.entity';
+import { UseGuards } from '@nestjs/common';
+
+import { IContext } from 'src/commons/interfaces/context';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+
+  @UseGuards(GqlAuthGuard('access')) // APi가기전 검증먼저 해줘
+  @Query(() => String)
+  fetchUser(
+    @Context() context: IContext, //
+  ): string {
+    console.log('==================');
+    console.log(context.req.user);
+    console.log('==================');
+
+    return '인가에 성공하였습니다. ';
+  }
+
+  @Mutation(() => User)
+  createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ): Promise<User> {
+    return this.usersService.create({ email, password, name, age });
+  }
+}
